42b51e10548d3cab2cbbaac1d240b6b2d026e517
Mykhaylo Gerasika <mgerasika@gmail.com>
2023-05-13 06:42:33 +0300

    Ignore no-uknown property into eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import 'twin.macro';
 
 function App(): JSX.Element {
-    return <div tw="h-full bg-black text-white">Hello world</div>;
+    return <div tw="h-screen bg-black text-white">Hello world</div>;
 }
 
 export default App;
